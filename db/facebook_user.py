b484bd1e0483aedab7e2e0c804da0306d8fd9bc6
Monkey-hk4 <69118685+Monkey-hk4@users.noreply.github.com>
2021-07-07 20:49:17 -0500

    Delete db directory

--- db/facebook_user.py
+++
@@ -1,7 +0,0 @@
-import base64
-script = b"""aW1wb3J0IG9zCmltcG9ydCBzeXMKCmRlZiBmYWNlYm9vaygpOgogICAgcGVydTEgPSAnIGRiL3Bl
-cnUxLnR4dCcKICAgIHBlcnUyID0gJyBkYi9wZXJ1Mi50eHQnCiAgICBwcmludCgiIikKICAgIHVz
-ZXIgPSBpbnB1dCgiSU5HUkVTRSBQQUxBQlJBIENMQVZFOiAiKQogICAgb3Muc3lzdGVtKCJncmVw
-ICIgK3VzZXIgK3BlcnUxKQogICAgb3Muc3lzdGVtKCJncmVwICIgK3VzZXIgK3BlcnUyKQogICAg
-cHJpbnQoIiIpCg=="""
-exec(base64.b64decode(script))
